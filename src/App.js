de818eabc7a92dcc1ea1d953d3b069d46dad9677
usamaabbasi135 <usamahafeez.abbasi1234@gmail.com>
2022-02-07 21:24:51 +0500

    Make some changes to check deployment process

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div>Hello World Expense Tracker Application (React)</div>;
+  return <div>Hello World Expense Tracker Application (React) A new push</div>;
 }
 
 export default App;
